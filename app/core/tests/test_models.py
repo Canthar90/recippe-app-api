2a23cc6e328cca815990b6e3f996f41c0e7fb9ef
unknown <UR@NB-NKL-023.wa.local>
2022-05-11 12:45:25 +0200

    Custom user Module

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_succesful(self):
+        """Test creating a new user with an emiail is succesfull"""
+        email = "example@example.com"
+        password = "example1!"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+
+    def test_new_user_email_normalized(self):
+        """test the email for a new user is normalized"""
+        email = 'test@SADASDWEQ.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "tests123")
+
+
+    def test_create_new_superuser(self):
+        """Test creating new superuser"""
+        user = get_user_model().objects.create_superuser(
+            "test@example.com",
+            "test123"
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
